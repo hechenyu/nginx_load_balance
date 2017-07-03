8ee367e33abf551a8d0b15e934e61b7b2af34e40
hexu <hexu_bupt@sina.com>
2017-07-03 23:08:11 +0800

    change bootstrap.sh

--- nginx-1.13.0/bootstrap.sh
+++ nginx-1.13.0/bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 $(cd lib/unordered_map_extern_c/lib && make clean && make)
-./configure --with-debug --with-stream --add-module=./src/stream/upstream_check/ && make
+#./configure --with-debug --with-stream --add-module=./src/stream/upstream_check/ && make
+./configure --without-http_gzip_module --without-http_rewrite_module --with-stream --add-module=./src/stream/upstream_check/ && make
